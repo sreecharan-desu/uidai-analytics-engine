dd63a8ada925582005cfe9faa9899f56ab50a272
sreecharan-desu <sreecharan309@gmail.com>
2026-01-10 17:58:43 +0530

    Add Swagger UI documentation

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,16 @@ app.use(express.json());
 app.use('/api', ingestRoutes);
 app.use('/api/insights', insightsRoutes);
 
+// Swagger Documentation
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+import path from 'path';
+
+const swaggerDocument = yaml.load(path.join(__dirname, '../swagger.yaml'));
+app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 app.get('/', (req, res) => {
-  res.send('UIDAI Data Sync Service Running');
+  res.redirect('/docs');
 });
 
 // Global error handler
